40164c1a66da729a7f353244a4ee60989aa43d91
Joao Moreira <jgmoreira1809@gmail.com>
2023-02-28 17:43:28 -0300

    Fix Req 06 OK

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -30,4 +30,5 @@ def get_unique_job_types(path: str) -> List[str]:
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
     jobs_by_type = [job for job in jobs if job["job_type"] == job_type]
+
     return jobs_by_type
