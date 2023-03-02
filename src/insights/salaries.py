482ac556a25c8028d7707fa26ac3272dbd0f19d3
Joao Moreira <jgmoreira1809@gmail.com>
2023-03-02 18:59:28 -0300

    Req 08 e 09 OK

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -27,47 +27,38 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
+    try:
+        if "min_salary" not in job or "max_salary" not in job:
+            raise ValueError
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
+        min_salary = int(job["min_salary"])
+        max_salary = int(job["max_salary"])
 
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
+        if min_salary > max_salary:
+            raise ValueError
 
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+        salary = int(salary)
+
+        salary_range = min_salary <= salary <= max_salary
+
+        return salary_range
+
+    except ValueError:
+        raise ValueError
 
 
 def filter_by_salary_range(
     jobs: List[dict],
     salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
+    jobs_filter = []
 
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                jobs_filter.append(job)
 
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
+        except ValueError:
+            continue
+
+    return jobs_filter
