1af23f98d0a9596461cc29479c5773d0d2d097c3
Joao Moreira <jgmoreira1809@gmail.com>
2023-03-02 19:29:15 -0300

    Req 10 fix again

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,5 +2,5 @@ from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    count = count_ocurrences("tests/mocks/jobs.csv", "developer")
-    assert count == 1
+    count = count_ocurrences("data/jobs.csv", "python")
+    assert count == 1639
